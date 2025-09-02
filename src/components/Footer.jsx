1a3cfca667e55c6854f32226a4f0cc7c66f71661
Daniela Martínez V <danmv.0991@gmail.com>
2025-09-02 01:28:45 +0000

    Terminar footer y colocar botones en las imágenes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,22 @@
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center" id="footerWeb">
-		
+		<div className="mainContainer">
+			<div className="dataBakery">
+				<p><b>Estamos ubicados en:</b> Av. Luis Vega y Morroy 230, zona dos extendida,
+				Balaustradas, 76079 Santiago de Querétaro, Qro. </p>
+				<br />
+				<p><b>Horarios de atención:</b> L-V de 7:30 am - 20:00 pm y Sábado de 8:30am - 18:30pm </p>
+				<br />
+				<p><i class="fa-solid fa-phone-flip"></i> 4426071353</p>
+				<br />
+				<p><i class="fa-solid fa-envelope"></i> casamarquesa@gmail.com</p>
+			</div>
+			<div className="socialMedia">
+				<p><b>Búcanos en nuestras redes como:</b></p>
+				<br />
+				<p></p>
+
+			</div>
+		</div>
 	</footer>
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -63,21 +63,21 @@ export const Home = () => {
 						<div className="carousel-item active">
 							<img src="src/assets/img/imagenes/cafe_croasant.jpg" className="d-block w-100" alt="Café y croasant" />
 							<div className="carousel-caption d-none d-md-block">
-								<h5>First slide label</h5>
+								<h5>CAFETERÍA</h5>
 								<p>Some representative placeholder content for the first slide.</p>
 							</div>
 						</div>
 						<div className="carousel-item">
-							<img src="src/assets/img/imagenes/frenteCafeteria.jpeg" className="d-block w-100" alt="Cafetería Casa Marquesa" />
+							<img src="src/assets/img/imagenes/mostradorCafe.jpg" className="d-block w-100" alt="Cafetería Casa Marquesa" />
 							<div className="carousel-caption d-none d-md-block">
-								<h5>Second slide label</h5>
+								<h5>CAFETERÍA</h5>
 								<p>Some representative placeholder content for the second slide.</p>
 							</div>
 						</div>
 						<div className="carousel-item">
 							<img src="src/assets/img/imagenes/cafe_baguete.jpg" className="d-block w-100" alt="Café americano y Baguette" />
 							<div className="carousel-caption d-none d-md-block">
-								<h5>Third slide label</h5>
+								<h5>CAFETERÍA</h5>
 								<p>Some representative placeholder content for the third slide.</p>
 							</div>
 						</div>
